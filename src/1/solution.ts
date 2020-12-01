1536a468856b34a817b4772c19bc6cc4b164ab60
Josep M Sobrepere <josepot@gmail.com>
2020-12-01 06:25:35 +0100

    Day 1 raw solution

--- src/1/solution.ts
+++ src/1/solution.ts
@@ -1,15 +1,24 @@
-import add from "utils/add"
+const solution1 = (lines: string[]) => {
+  const entries = lines.map(Number)
 
-const getFuel = (x: number) => Math.floor(x / 3) - 2
-const getRecursiveFuel = (x: number): number => {
-  const fuel = getFuel(x)
-  return fuel < 1 ? 0 : fuel + getRecursiveFuel(fuel)
+  for (let i = 0; i < entries.length; i++) {
+    for (let z = i + 1; z < entries.length; z++) {
+      if (entries[i] + entries[z] === 2020) return entries[i] * entries[z]
+    }
+  }
 }
 
-const solution1 = (lines: string[]) =>
-  lines.map(Number).map(getFuel).reduce(add)
+const solution2 = (lines: string[]) => {
+  const entries = lines.map(Number)
 
-const solution2 = (lines: string[]) =>
-  lines.map(Number).map(getRecursiveFuel).reduce(add)
+  for (let i = 0; i < entries.length; i++) {
+    for (let z = i + 1; z < entries.length; z++) {
+      for (let zz = z + 1; zz < entries.length; zz++) {
+        if (entries[i] + entries[z] + entries[zz] === 2020)
+          return entries[i] * entries[z] * entries[zz]
+      }
+    }
+  }
+}
 
 export default [solution1, solution2]
