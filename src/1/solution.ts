cc485ebf3006924685b1de86616b410f6e5c0ab0
Josep M Sobrepere <josepot@gmail.com>
2020-12-02 08:20:39 +0100

    Extra cleanup day 1

--- src/1/solution.ts
+++ src/1/solution.ts
@@ -1,13 +1,13 @@
+import { linesMapper } from "utils/linesMapper"
 import { tupleCombinations } from "utils/tupleCombinations"
+import { multiply } from "utils/multiply"
 
-const solution1 = (lines: string[]) =>
-  tupleCombinations(lines.map(Number), (a, b) => a + b === 2020)!.reduce(
-    (a, b) => a * b,
-  )
+const solution1 = linesMapper(Number, (lines) =>
+  tupleCombinations(lines, (a, b) => a + b === 2020)!.reduce(multiply),
+)
 
-const solution2 = (lines: string[]) =>
-  tupleCombinations(lines.map(Number), (a, b, c) => a + b + c === 2020)!.reduce(
-    (a, b) => a * b,
-  )
+const solution2 = linesMapper(Number, (lines) =>
+  tupleCombinations(lines, (a, b, c) => a + b + c === 2020)!.reduce(multiply),
+)
 
 export default [solution1, solution2]
