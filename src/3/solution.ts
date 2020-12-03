0d07e9514a8217710477af3ec4ce8b6348cb6e1d
Josep M Sobrepere <josepot@gmail.com>
2020-12-03 07:11:00 +0100

    Day 3 extra cleanup

--- src/3/solution.ts
+++ src/3/solution.ts
@@ -3,30 +3,27 @@ import { multiply } from "utils/multiply"
 
 const lineMapper = (line: string) => line.split("").map((x) => x === "#")
 
-const solution1 = linesMapper(
-  lineMapper,
-  (grid) =>
-    grid.slice(1).filter((row, idx) => row[((idx + 1) * 3) % row.length])
-      .length,
-)
+const countTrees = (grid: boolean[][], xDelta: number, yDelta: number) => {
+  let counter = 0
+  const nRows = grid.length
+  const nCols = grid[0].length
+  for (let y = yDelta, x = xDelta; y < nRows; y += yDelta, x += xDelta) {
+    if (grid[y][x % nCols]) counter++
+  }
+  return counter
+}
 
-const solution2 = linesMapper(lineMapper, (grid) => {
-  const deltas = [
+const solution1 = linesMapper(lineMapper, (grid) => countTrees(grid, 3, 1))
+
+const solution2 = linesMapper(lineMapper, (grid) =>
+  [
     [1, 1],
     [3, 1],
     [5, 1],
     [7, 1],
     [1, 2],
   ]
-
-  return deltas
-    .map(
-      ([xDelta, yDelta]) =>
-        grid.slice(yDelta).filter((row, idx) => {
-          if (idx % yDelta === 1) return false
-          return row[((idx / yDelta + 1) * xDelta) % row.length]
-        }).length,
-    )
-    .reduce(multiply)
-})
+    .map(([xDelta, yDelta]) => countTrees(grid, xDelta, yDelta))
+    .reduce(multiply),
+)
 export default [solution1, solution2]
