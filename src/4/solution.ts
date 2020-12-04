78d280d81428e9cdbd2585fc74be62720b98715f
Josep M Sobrepere <josepot@gmail.com>
2020-12-04 06:46:42 +0100

    Day 4 raw

---
+++ src/4/solution.ts
@@ -0,0 +1,122 @@
+import { linesMapper } from "utils/linesMapper"
+
+const lineMapper = (line: string) => line
+
+const fields: Record<string, (val: string) => boolean> = {
+  byr: (x) => {
+    const val = Number(x)
+    return val >= 1920 && val <= 2002
+  },
+  iyr: (x) => {
+    const val = Number(x)
+    return val >= 2010 && val <= 2020
+  },
+  eyr: (x) => {
+    const val = Number(x)
+    return val >= 2020 && val <= 2030
+  },
+  hgt: (x) => {
+    const val = parseInt(x, 10)
+    if (x.endsWith("cm")) {
+      return val >= 150 && val <= 193
+    } else if (x.endsWith("in")) {
+      return val >= 59 && val <= 76
+    } else {
+      return false
+    }
+  },
+  hcl: (x) => {
+    if (!x.startsWith("#")) return false
+    return (
+      x
+        .slice(1)
+        .split("")
+        .map((x) => x.charCodeAt(0))
+        .filter((x) => (x >= 97 && x <= 102) || (x >= 48 && x <= 57)).length ===
+      6
+    )
+  },
+  ecl: (x) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(x),
+  pid: (x) => {
+    return (
+      x
+        .split("")
+        .map((x) => x.charCodeAt(0))
+        .filter((x) => x >= 48 && x <= 57).length === 9
+    )
+  },
+  cid: () => true,
+}
+
+const solution1 = linesMapper(lineMapper, (lines) => {
+  let rawPassports: string[] = []
+  let [current] = lines
+  current += " "
+  lines.slice(1).forEach((line) => {
+    if (line.length === 0) {
+      console.log(current)
+      rawPassports.push(current)
+      current = ""
+    }
+    current += line
+    current += " "
+  })
+  if (current) {
+    rawPassports.push(current)
+  }
+  const passports = rawPassports.map((rawPass) =>
+    Object.fromEntries(
+      rawPass
+        .split(" ")
+        .filter(Boolean)
+        .map((fieldVal) => {
+          const [field, val] = fieldVal.split(":")
+          return [field, val]
+        }),
+    ),
+  )
+  const isValid = (pass: Record<string, string>) => {
+    pass.cid = "whatever"
+    return Object.keys(pass).filter((key) => fields[key]).length >= 8
+  }
+  return passports.filter(isValid).length
+})
+
+const solution2 = linesMapper(lineMapper, (lines) => {
+  let rawPassports: string[] = []
+  let [current] = lines
+  current += " "
+  lines.slice(1).forEach((line) => {
+    if (line.length === 0) {
+      console.log(current)
+      rawPassports.push(current)
+      current = ""
+    }
+    current += line
+    current += " "
+  })
+  if (current) {
+    rawPassports.push(current)
+  }
+  const passports = rawPassports.map((rawPass) =>
+    Object.fromEntries(
+      rawPass
+        .split(" ")
+        .filter(Boolean)
+        .map((fieldVal) => {
+          const [field, val] = fieldVal.split(":")
+          return [field, val]
+        }),
+    ),
+  )
+  const isValid = (pass: Record<string, string>) => {
+    pass.cid = "whatever"
+    return (
+      Object.entries(pass).filter(([key, value]) => fields[key](value))
+        .length === 8
+    )
+  }
+  return passports.filter(isValid).length
+})
+
+export default [solution1, solution2]
