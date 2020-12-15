2adb6f28b7e32f89fea11e16e4d4641fd0ff9306
Josep M Sobrepere <josepot@gmail.com>
2020-12-15 08:48:08 +0100

    Day 15 cleanup

--- src/15/solution.ts
+++ src/15/solution.ts
@@ -1,33 +1,19 @@
-interface Node {
-  prev: Node | null
-  turn: number
-}
 const solution = (target: number) => (line: string) => {
-  const cache = new Map<number, Node>()
+  const input = line.split(",").map(Number)
+  const cache = new Map<number, number>()
   let turn = 1
-  let last = 0
 
-  line
-    .split(",")
-    .map(Number)
-    .forEach((n) => {
-      cache.set((last = n), { turn: turn++, prev: null })
-    })
+  input.slice(0, -1).forEach((n) => {
+    cache.set(n, turn++)
+  })
 
+  let next = input[input.length - 1]
   do {
-    const f = cache.get(last)!.prev?.turn ?? 0
-    last = f === 0 ? 0 : turn - 1 - f
-
-    if (turn === target) return last
-    const nextNode = cache.get(last)
-    if (nextNode) {
-      nextNode.prev = null
-      cache.set(last, { turn, prev: nextNode })
-    } else {
-      cache.set(last, { turn, prev: null })
-    }
-    turn++
-  } while (true)
+    let nextNext = cache.has(next) ? turn - cache.get(next)! : 0
+    cache.set(next, turn)
+    next = nextNext
+  } while (++turn < target)
+  return next
 }
 
 export default [2020, 30000000].map(solution)
