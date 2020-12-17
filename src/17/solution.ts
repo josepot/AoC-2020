4219df2f64689e4dcfbf6462219c81eb8726bcd1
Josep M Sobrepere <josepot@gmail.com>
2020-12-17 07:32:20 +0100

    Day 17 part 2 raw

--- src/17/solution.ts
+++ src/17/solution.ts
@@ -1,24 +1,31 @@
-const deltas: [number, number, number][] = []
+const deltas: [number, number, number, number][] = []
 
-for (let z = -1; z < 2; z++) {
-  for (let x = -1; x < 2; x++) {
-    for (let y = -1; y < 2; y++) {
-      if (x !== 0 || y !== 0 || z !== 0) deltas.push([x, y, z])
+for (let w = -1; w < 2; w++) {
+  for (let z = -1; z < 2; z++) {
+    for (let x = -1; x < 2; x++) {
+      for (let y = -1; y < 2; y++) {
+        if (x !== 0 || y !== 0 || z !== 0 || w !== 0) deltas.push([x, y, z, w])
+      }
     }
   }
 }
 
 const fromIdToPos = (id: string) => {
-  return id.split(",").map(Number) as [number, number, number]
+  return id.split(",").map(Number) as [number, number, number, number]
 }
 
-const fromPosToId = (x: number, y: number, z: number) => {
-  return [x, y, z].join(",")
+const fromPosToId = (x: number, y: number, z: number, w: number) => {
+  return [x, y, z, w].join(",")
 }
 
 interface Grid {
   positions: Map<string, boolean>
-  dimensions: [[number, number], [number, number], [number, number]]
+  dimensions: [
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number],
+  ]
 }
 
 const getNeighbours = (id: string, grid: Grid) => {
@@ -31,6 +38,7 @@ const getNeighbours = (id: string, grid: Grid) => {
             delta[0] + position[0],
             delta[1] + position[1],
             delta[2] + position[2],
+            delta[3] + position[3],
           ),
         ) ?? false,
     )
@@ -43,6 +51,7 @@ const nextGrid = (grid: Grid) => {
       [number, number],
       [number, number],
       [number, number],
+      [number, number],
     ],
     positions: new Map<string, boolean>(),
   }
@@ -51,12 +60,14 @@ const nextGrid = (grid: Grid) => {
   for (let x = dimensions[0][0]; x < dimensions[0][1]; x++) {
     for (let y = dimensions[1][0]; y < dimensions[1][1]; y++) {
       for (let z = dimensions[2][0]; z < dimensions[2][1]; z++) {
-        const id = fromPosToId(x, y, z)
-        const nNeighActive = getNeighbours(id, grid)
-        if (grid.positions.get(id)) {
-          result.positions.set(id, nNeighActive === 2 || nNeighActive === 3)
-        } else {
-          result.positions.set(id, nNeighActive === 3)
+        for (let w = dimensions[3][0]; w < dimensions[3][1]; w++) {
+          const id = fromPosToId(x, y, z, w)
+          const nNeighActive = getNeighbours(id, grid)
+          if (grid.positions.get(id)) {
+            result.positions.set(id, nNeighActive === 2 || nNeighActive === 3)
+          } else {
+            result.positions.set(id, nNeighActive === 3)
+          }
         }
       }
     }
@@ -70,6 +81,7 @@ const solution1 = (lines: string[]) => {
       [0, 8],
       [0, 8],
       [0, 1],
+      [0, 1],
     ],
     positions: new Map<string, boolean>(),
   }
@@ -79,7 +91,7 @@ const solution1 = (lines: string[]) => {
       .split("")
       .map((d) => d === "#")
       .forEach((isActive, x) => {
-        grid.positions.set([x, y, 0].join(","), isActive)
+        grid.positions.set([x, y, 0, 0].join(","), isActive)
       })
   })
 
